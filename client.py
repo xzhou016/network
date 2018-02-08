498766ee9bb7d04c5e874a1371a6404a83e1fa75
Xiao Zhou <xzhou016@wch129-08.cs.ucr.edu>
2018-02-08 10:29:01 -0800

    UPDATE: spaces to tabs

--- client.py
+++ client.py
@@ -2,11 +2,11 @@ import socket   #for sockets
 import sys  #for exit
  
 try:
-    #create an AF_INET, STREAM socket (TCP)
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	#create an AF_INET, STREAM socket (TCP)
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 except socket.error, msg:
-    print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
-    sys.exit();
+	print 'Failed to create socket. Error code: ' + str(msg[0]) + ' , Error message : ' + msg[1]
+	sys.exit();
  
 print 'Socket Created'
  
@@ -16,13 +16,13 @@ port = 8880
 #port = 80
 
 try:
-    remote_ip = socket.gethostbyname( host )
+	remote_ip = socket.gethostbyname( host )
  
 except socket.gaierror:
-    #could not resolve
-    print 'Hostname could not be resolved. Exiting'
-    sys.exit()
-     
+	#could not resolve
+	print 'Hostname could not be resolved. Exiting'
+	sys.exit()
+	
 print 'Ip address of ' + host + ' is ' + remote_ip
  
 #Connect to remote server
@@ -34,12 +34,12 @@ print 'Socket Connected to ' + host + ' on ip ' + remote_ip
 message = "GET / HTTP/1.1\r\n\r\n"
 
 try :
-    #Set the whole string
-    s.sendall(message)
+	#Set the whole string
+	s.sendall(message)
 except socket.error:
-    #Send failed
-    print 'Send failed'
-    sys.exit()
+	#Send failed
+	print 'Send failed'
+	sys.exit()
  
 print 'Message send successfully'
  
--- forwarder.py
+++ forwarder.py
@@ -14,57 +14,58 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print 'Socket created'
  
 try:
-    s.bind((HOST, PORT))
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
+	s.bind((HOST, PORT))
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
 except socket.error , msg:
-    print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-    sys.exit()
-    
+	print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
+	sys.exit()
+	
 print 'Socket bind complete'
 
 s.listen(10)
 print 'Socket now listening'
 
+#connect to server
+sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+remote_ip = socket.gethostbyname(host)
+sc.connect((remote_ip, SERVER_PORT))
+
 
 #Function for handling connections. This will be used to create threads
 def clientthread(conn):
-     
-    #infinite loop so that function do not terminate and thread do not end.
-    while True:
-         
-        #Receiving from client
-        data = conn.recv(1024)
-        if data[0:2] == '!fw':
-           reply = 'Ok from forwarder...' + data
-	   conn.sendall(reply)
-        elif data[0:7] == '!report':
-            for member in myList:
-                member.sendall(member)
-        else:
-            reply = 'Received data from forwarder ' + data
-            start_new_thread(forwardtoserverthread ,(reply,))
-    #came out of loop
-    conn.close()
-    myList.remove(conn)
+	#Sending message to connected client
+	conn.send('Welcome to the forwarder. Type something and hit enter\n')
+	#infinite loop so that function do not terminate and thread do not end.
+	while True:
+		
+		#Receiving from client
+		data = conn.recv(1024)
+		if data[0:2] == '!fw':
+			reply = 'Ok from forwarder...' + data
+		conn.sendall(reply)
+		elif data[0:7] == '!report':
+			for member in myList:
+				member.sendall(member)
+		else:
+			reply = 'Received data from forwarder ' + data
+			start_new_thread(forwardtoserverthread ,(reply,))
+	#came out of loop
+	conn.close()
+	myList.remove(conn)
 
 #Forwarding thread
 def forwardtoseverthread(msg):
-    sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    remote_ip = socket.gethostbyname(host)
-    sc.connect((remote_ip, SERVER_PORT))
-    sc.sendall(msg)
+	sc.sendall(msg)
+	sc.close()
  
 #now keep talking with the client
 while 1:
-    #Sending message to connected client
-    conn.send('Welcome to the forwarder. Type something and hit enter\n')
-    #wait to accept a connection - blocking call
-    #wait to accept a connection - blocking call
-    conn, addr = s.accept()
-    myList.append(conn)
-    print 'Connected with ' + addr[0] + ':' + str(addr[1])
-    #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
-    start_new_thread(clientthread ,(conn,))
-    print myList
+	#wait to accept a connection - blocking call
+	conn, addr = s.accept()
+	myList.append(conn)
+	print 'Connected with ' + addr[0] + ':' + str(addr[1])
+	#start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
+	start_new_thread(clientthread ,(conn,))
+	print myList
 
 s.close()
