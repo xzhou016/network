317117b4933e19196363d7d0282aba6690e58428
xzhou016 <xzhou016@ucr.edu>
2018-03-01 18:24:43 -0800

    Started final project

---
+++ bridges.py
@@ -0,0 +1,20 @@
+from mininet.topo import Topo
+
+class Bridges(Topo):
+	def __init__(self):
+		"Create custom topo."
+		#Init topo
+		Topo.__init__(self)
+		#Add hosts and switches
+		h1 = self.addHost('h1')
+		h2 = self.addHost('h2')
+		h3 = self.addHost('h3')
+		h4 = self.addHost('h4')
+		switch = self.addSwitch('s1')
+		#Add links
+		self.addLink(h1, switch)
+		self.addLink(h2, switch)
+		self.addLink(switch, h3)
+		self.addLink(switch, h4)
+
+topos = {'bridges': (lambda: Bridges())}
--- forwarder.py
+++ forwarder.py
@@ -1,20 +1,27 @@
 import socket
 import sys
 from thread import *
+from sys import argv
  
 HOST = 'localhost'   #  available interfaces
-PORT = 8880 # my port
+print argv
+#PORT = int(argv[1]) # my port
 SERVER_PORT = 8888
+myip = socket.gethostbyaddr(argv[1])
+mymac = argv[2]
+myport= int(argv[3])
 
 
 #Define a new list
-myList = []
-dataList = [] 
+arp = ['10.0.100.3', '08:00:27:58:32:0d', 60, 8001]
+arpList = [] 
+broadCastList = [8000, 8001, 8002, 8003]
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print 'Socket created'
  
 try:
-	s.bind((HOST, PORT))
+	s.bind((myip, myport))
 	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
 except socket.error , msg:
 	print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
@@ -27,7 +34,7 @@ print 'Socket now listening'
 
 #connect to server
 sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-remote_ip = socket.gethostbyname(HOST)
+#remote_ip = socket.gethostbyname(myip)
 
 
 #Function for handling connections. This will be used to create threads
@@ -39,29 +46,39 @@ def clientthread(conn):
 		#Receiving from client
 		data = conn.recv(1024)
 		data = data.split()
-		#print data
+		print data
 		#param, send_host ,send_port, msg = data.split()
-		if data[0]  == '!fw':
-			#connect to server
+#		if data[0]  == '!fw':
+#			#connect to server
+#			sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#			remote_ip = socket.gethostbyname(data[1])
+#			reply = 'Ok from forwarder...' + data[3] + '\n'
+#			conn.sendall(reply)
+#			sc.connect((remote_ip, int(data[2])))
+#			sc.sendall(data[3]+ '\n')
+#			dataList.append(str(HOST))
+#i			dataList.append(str(PORT))
+#			dataList.append(str(sc.getsockname()))
+#			dataList.append(data[3]+ '\n')
+#			myList.append(dataList)
+#			sc.close()
+#		elif data[0]  == '!report':
+#			for i in myList:
+#				print i	
+		if data[0] == 'pingmac':
 			sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			remote_ip = socket.gethostbyname(data[1])
-			reply = 'Ok from forwarder...' + data[3] + '\n'
-			conn.sendall(reply)
-			sc.connect((remote_ip, int(data[2])))
-			sc.sendall(data[3]+ '\n')
-			dataList.append(str(HOST))
-			dataList.append(str(PORT))
-			dataList.append(str(sc.getsockname()))
-			dataList.append(data[3]+ '\n')
-			myList.append(dataList)
-			sc.close()
-		elif data[0]  == '!report':
-			for i in myList:
-				print i	
-		elif data[0]  == '!q':
+			remote_ip = socket.gethostbyname(arp[1])
+			reply = 'arp '
+			#conn.sendall(reply)
+			sc.connect((remote_ip, arp[4]))
+			sc.sendall(reply)			
+
+		if data[0]  == '!q':
 			break
 	#came out of loop
 	conn.close()
+	s.close()
+	sys.exit()
 
 #now keep talking with the client
 while 1:
