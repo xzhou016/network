ccbaaa04734fbb833f997579f7f296d0b6916245
xzhou016 <xzhou016@ucr.edu>
2018-03-08 19:56:11 -0800

    WIP

--- protocol.py
+++ protocol.py
@@ -2,46 +2,61 @@ import socket
 import sys
 from thread import *
 from sys import argv
+import commands
+from uuid import getnode as get_mac
  
-HOST = 'localhost'   #  available interfaces
-print argv
+HOST = argv[1]   #  available interfaces
+ips = commands.getoutput("/sbin/ifconfig | grep -i \"inet\" | awk '{print $2}'")
+print ips
 #PORT = int(argv[1]) # my port
 SERVER_PORT = 8888
-myip = socket.gethostbyaddr(argv[1])
+#myip = socket.gethostbyaddr(argv[1])
+#print myip
 #mymac = argv[2]
 #myport= int(argv[3])
 
 
 #Define a new list
-arp = ['10.0.100.3', '08:00:27:58:32:0d', 60, 8001]
-arpList = [] 
-broadCastList = [8000, 8001, 8002, 8003]
+portList = [1, 2, 3]
 
 #Create 4 sockets
 s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s4 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print 'Socket created' 
-print  (s1 , s2 ,s3 ,s4)  
- 
-try:
-#	s1.bind((myip, arp[3])
-#	s.bind((myip, myport)
-	s1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	s1.bind((myip, SERVER_PORT)
-except socket.error , msg:
-	print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-	sys.exit()
-	
-print 'Socket bind complete'
+print  (s1 , s2 ,s3 )
 
+#Bind all sockets
+try:
+	s1.bind((HOST, portList[0]))
+	s2.bind((HOST, portList[1]))
+	s3.bind((HOST, portList[2]))
+	s1.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+	s2.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+	s3.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR,1)
+except socket.error, msg:
+	print 'Bind failed. Error code: ' + str(msg[0]) + '\nMessage: ' + msg[1]
+	sys.exit()
+print 'Socket bind complete' 
+
+#Set all pocket to listen
 s1.listen(10)
+s2.listen(10)
+s3.listen(10)
 print 'Socket now listening'
 
-#connect to server
-sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#remote_ip = socket.gethostbyname(myip)
+
+#Generate Bridge Protocol Data Unit
+myPriority = '32768:'
+myMac = commands.getoutput("/sbin/ifconfig | grep -i \"HWaddr\" | awk '{print $5}'")
+BPDU = [myPriority, myMac, myPriority + myMac]
+print BPDU
+#print 'BPDU as int: '
+#print get_mac()
+
+def broadcast(conn):
+	
+
 
 
 #Function for handling connections. This will be used to create threads
@@ -54,24 +69,6 @@ def clientthread(conn):
 		data = conn.recv(1024)
 		data = data.split()
 		print data
-		#param, send_host ,send_port, msg = data.split()
-#		if data[0]  == '!fw':
-#			#connect to server
-#			sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#			remote_ip = socket.gethostbyname(data[1])
-#			reply = 'Ok from forwarder...' + data[3] + '\n'
-#			conn.sendall(reply)
-#			sc.connect((remote_ip, int(data[2])))
-#			sc.sendall(data[3]+ '\n')
-#			dataList.append(str(HOST))
-#i			dataList.append(str(PORT))
-#			dataList.append(str(sc.getsockname()))
-#			dataList.append(data[3]+ '\n')
-#			myList.append(dataList)
-#			sc.close()
-#		elif data[0]  == '!report':
-#			for i in myList:
-#				print i	
 		if data[0] == 'pingmac':
 			sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			remote_ip = socket.gethostbyname(arp[1])
@@ -85,6 +82,8 @@ def clientthread(conn):
 	#came out of loop
 	conn.close()
 	s1.close()
+	s2.close()
+	s3.close()
 	sys.exit()
 
 #now keep talking with the client
