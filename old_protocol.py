bd5f6e445e4b4329564b224b91b795b7b8d23c91
xzhou016 <xzhou016@ucr.edu>
2018-03-15 08:44:14 -0700

    added some examples

--- old_protocol.py
+++ old_protocol.py
@@ -19,10 +19,10 @@ SERVER_PORT = 8888
 #Define a new list
 portList = [1, 2, 3]
 
-#Create 3 sockets
-s1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#Create 4 sockets
+s1 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print 'Socket created' 
 print  (s1 , s2 ,s3 )
 
@@ -39,6 +39,13 @@ except socket.error, msg:
 	sys.exit()
 print 'Socket bind complete' 
 
+#Set all pocket to listen
+s1.listen(10)
+s2.listen(10)
+s3.listen(10)
+print 'Socket now listening'
+
+
 #Generate Bridge Protocol Data Unit
 myPriority = '32768:'
 myMac = commands.getoutput("/sbin/ifconfig | grep -i \"HWaddr\" | awk '{print $5}'")
@@ -47,32 +54,45 @@ print BPDU
 #print 'BPDU as int: '
 #print get_mac()
 
-#Set all pocket to listen
-#s1.listen(10)
-#s2.listen(10)
-#s3.listen(10)
-def listenport(s):
-	s.listen(10)
-	print 'Socket now listening'
+def broadcast(conn):
 	
-def decode(msg):
-	msg_array = msg.split(':')
-	priority = msg.pop(0)
-	for i in msg:
-		mac += i
-	if 
 
-def clientthread(rconn):
-	print 'Client created'
+
+
+#Function for handling connections. This will be used to create threads
+def clientthread(conn):
+	#Sending message to connected client
+	conn.send('Welcome to the forwarder. Type something and hit enter\n')
+	#infinite loop so that function do not terminate and thread do not end.
 	while True:
-		rconn.sendall('BPDU')
-		response = conn.recv(2048)
-	rconn.close()
+		#Receiving from client
+		data = conn.recv(1024)
+		data = data.split()
+		print data
+		if data[0] == 'pingmac':
+			sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			remote_ip = socket.gethostbyname(arp[1])
+			reply = 'arp '
+			#conn.sendall(reply)
+			sc.connect((remote_ip, arp[4]))
+			sc.sendall(reply)			
 
-start_new_thread(listenport,(s1,))
+		if data[0]  == '!q':
+			break
+	#came out of loop
+	conn.close()
+	s1.close()
+	s2.close()
+	s3.close()
+	sys.exit()
 
+#now keep talking with the client
 while 1:
-	
+	#wait to accept a connection - blocking call
 	conn, addr = s1.accept()
 	print 'Connected with ' + addr[0] + ':' + str(addr[1])
+	#start new thread takes 1st argument as a function name to be run, 
+	#second is the tuple of arguments to the function.
 	start_new_thread(clientthread ,(conn,))
+
+print save
---
+++ testcom.py
@@ -0,0 +1,2 @@
+import commands
+ips = commands.getoutput("
